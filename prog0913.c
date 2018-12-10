d513033cc45b53dc4d270b7a08111d0f25ff866e
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-12-10 15:02:07 +0900

    added prog0913

--- prog0913.c
+++ prog0913.c
@@ -11,29 +11,29 @@ struct DATA {
 struct DATA eval( int p[] )
 {
 	struct DATA result;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	int max=0,min=100,sum=0;
+	double avg;
+	int i=0;
+	while(p[i] != -1){
+		if(max < p[i]){
+			max = p[i];
+		}
+		if(min > p[i]){
+			min = p[i];
+		}
+		sum += p[i];
+		i++;
+	}
+	avg = (double)sum/i;
+	result.min = min;
+	result.max = max;
+	result.avg = avg;
 	return result;
 }
 
 void output(struct DATA *s)
 {
-
-
-
-
+	printf("mib:%d,max:%d,avg:%.1f\n",s->min,s->max,s->avg);
 }
 
 int main( void )
