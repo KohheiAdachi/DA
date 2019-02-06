00cd7930e69db46f5a5c81226b493cc43e7190c6
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2019-02-07 01:06:37 +0900

    ファイル移動

---
+++ test/hiyoko.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i=0,j=0;
+    printf("%d",i);
+    printf("%d",j);
+    ++j;
+    i++;
+    printf("%d",i);
+    printf("%d",j);
+}
