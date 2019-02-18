a043a43f8d848a71b11b73cc0f886ccf6b03bc74
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2019-02-18 16:41:26 +0900

    qsort表示

--- prog0403.c
+++ prog0403.c
@@ -2,6 +2,14 @@
 
 #include <stdio.h>
 
+void print_array(int data[], int n)
+{
+    int i;
+    for( i=0 ; i<n ; i++ )
+        printf( "%d ", data[i] );
+    printf( "\n" );
+}
+
 void quick_sort(int data[], int left, int right)
 {
     int i, j,pivot,tmp;
@@ -11,7 +19,7 @@ void quick_sort(int data[], int left, int right)
     i = left;
     j = right;
     pivot = data[ (i+j)/2 ];
-
+    printf("pivot = %d\n",pivot);
 
     while( 1 ){
         while( data[i] < pivot )
@@ -25,18 +33,25 @@ void quick_sort(int data[], int left, int right)
         data[j] = tmp;
         i++;
         j--;
+        // printf("i=%d\n",i);
+        // printf("j=%d\n",j);
     }
+    print_array(data,8);
+    printf("i=%d\n",i);
+    printf("j=%d\n",j);
     quick_sort(data, left, i-1);
+    printf("q_sort:%d,%d\n",j+1,right);
     quick_sort(data, j+1, right);
 }
 
 int main(void)
 {
-    int i, data[15] = {6,3,1,5,7,3,4,9,2,8,0,2,8,5,1};
+    int i;
+    // int data[15] = {6,3,1,5,7,3,4,9,2,8,0,2,8,5,1};
+    int data[8] = { 4, 2, 7, 8, 1, 3, 6, 5 };
+    quick_sort(data, 0, 7);
 
-    quick_sort(data, 0, 14);
-
-    for( i=0; i<15; i++ )
+    for( i=0; i<7; i++ )
 	    printf("%d ", data[i]);
 
     return 0;
